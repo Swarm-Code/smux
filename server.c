31fa9e3a9c14268c7cdf1cc68d2494cefd2b35a1
rinadelph <rinadelph@users.noreply.github.com>
2025-10-15 12:40:44 -0400

    refactor: Refactor server.c
    Technical Details:
    - Tool: Edit
    - File: server.c
    - Session: d9d55975
    - Timestamp: Invalid Date
    - mem: Captured in audit trail

--- server.c
+++ server.c
@@ -216,9 +216,9 @@ server_start(struct tmuxproc *client, uint64_t flags, struct event_base *base,
 	RB_INIT(&all_window_panes);
 	TAILQ_INIT(&clients);
 	RB_INIT(&sessions);
-	/* RB_INIT(&projects); */ /* Temporarily disabled to debug hang */
-	/* next_project_id = 0; */ /* Temporarily disabled to debug hang */
-	/* plugin_init(); */ /* Temporarily disabled to debug hang */
+	RB_INIT(&projects);
+	next_project_id = 0;
+	plugin_init();
 
 	key_bindings_init();
 
